c6d7ee6b3ce7bdb25ea6594c6c88030bfb00e50e
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-05 16:10:02 +0100

    Added fabric world load listener

---
+++ fabric/src/main/java/com/rabbitminers/trackmap/fabric/events/TrackMapEventsFabric.java
@@ -0,0 +1,18 @@
+package com.rabbitminers.trackmap.fabric.events;
+
+import com.rabbitminers.trackmap.events.TrackMapCommonEvents;
+import com.simibubi.create.events.CommonEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.world.level.LevelAccessor;
+
+import java.util.concurrent.Executor;
+
+public class TrackMapEventsFabric {
+    public static void onLoadWorld(Executor executor, LevelAccessor world) {
+        TrackMapCommonEvents.onWorldLoad(world);
+    }
+
+    public static void register() {
+        ServerWorldEvents.LOAD.register(CommonEvents::onLoadWorld);
+    }
+}
