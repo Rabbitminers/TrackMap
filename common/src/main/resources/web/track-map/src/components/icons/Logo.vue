02f747b74882957857e190a6e68ec1938b301796
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-06 21:34:44 +0100

    Added Logo Component

---
+++ common/src/main/resources/web/track-map/src/components/icons/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+    <img :src="src" alt="Logo" />
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    size: {
+      type: String,
+      default: "m",
+      validator(value: string) {
+        return ["xs", "s", "m", "l", "xl"].includes(value);
+      },
+    },
+  },
+  data() {
+    return {
+      src: `/logo/${this.size}.png`,
+    };
+  },
+};
+</script>
