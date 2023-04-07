62152a3d048cb0d89938cea9808df60b36529d7e
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-08 00:00:06 +0100

    Added Routes To Server Initialisation

--- common/src/main/java/com/rabbitminers/trackmap/events/TrackMapCommonEvents.java
+++ common/src/main/java/com/rabbitminers/trackmap/events/TrackMapCommonEvents.java
@@ -1,19 +1,26 @@
 package com.rabbitminers.trackmap.events;
 
 import com.rabbitminers.trackmap.TrackMap;
+import com.rabbitminers.trackmap.http.HttpServer;
 import com.rabbitminers.trackmap.http.TrackMapServer;
+import com.rabbitminers.trackmap.http.routes.ConnectionRoutes;
+import com.rabbitminers.trackmap.http.routes.NodeRoutes;
+import com.rabbitminers.trackmap.http.routes.SiteRoutes;
 import net.minecraft.world.level.LevelAccessor;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class TrackMapCommonEvents {
     public static void onWorldLoad(LevelAccessor levelAccessor) {
-        new Thread(() -> {
-            try {
-                new TrackMapServer(8080);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
+        try {
+            HttpServer server = new HttpServer(8080);
+            ConnectionRoutes.init(server);
+            NodeRoutes.init(server);
+            SiteRoutes.init(server);
+            server.start();
+        } catch (IOException e) {
+            TrackMap.LOGGER.error("Failed to start server " + e);
+        }
     }
 }
