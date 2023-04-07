3ef68c18f366883df7db5b7a39ba935d592a79f9
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-07 23:57:20 +0100

    Re-implemented home routes

---
+++ common/src/main/java/com/rabbitminers/trackmap/http/routes/SiteRoutes.java
@@ -0,0 +1,36 @@
+package com.rabbitminers.trackmap.http.routes;
+
+import com.rabbitminers.trackmap.http.HttpServer;
+import com.rabbitminers.trackmap.http.util.HttpHelper;
+
+import java.io.InputStream;
+
+public class SiteRoutes {
+    public static HttpServer.HttpHandler home = (out, method, params) -> {
+        if (method.equals("GET")) {
+            InputStream fileStream = HomeRoute.class.getClassLoader().getResourceAsStream("web/index.html");
+            if (fileStream == null) {
+                HttpHelper.writeFileNotFound(out);
+                return;
+            }
+            byte[] fileBytes = fileStream.readAllBytes();
+            fileStream.close();
+
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            out.write("Content-Type: text/html\r\n".getBytes());
+            out.write(("Content-Length: " + fileBytes.length + "\r\n").getBytes());
+            out.write("\r\n".getBytes());
+
+            out.write(fileBytes);
+        } else {
+            out.write("HTTP/1.1 405 Method Not Allowed\r\n".getBytes());
+            out.write("\r\n".getBytes());
+            out.flush();
+        }
+    };
+
+    public static void init(HttpServer server) {
+        server.addHandler("/", SiteRoutes.home);
+        server.addHandler("/index", SiteRoutes.home);
+    }
+}
