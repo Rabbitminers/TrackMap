88ceabfeef9132cec4ca302e68b5ae7558884434
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-07 23:59:49 +0100

    Re-implemented node routes

---
+++ common/src/main/java/com/rabbitminers/trackmap/http/routes/NodeRoutes.java
@@ -0,0 +1,59 @@
+package com.rabbitminers.trackmap.http.routes;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.rabbitminers.trackmap.http.HttpServer;
+import com.rabbitminers.trackmap.tracks.TrackGraphSerializer;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class NodeRoutes {
+    public static HttpServer.HttpHandler allNodes = (out, method, params) -> {
+        if (method.equals("GET")) {
+            JsonArray data = TrackGraphSerializer.serializeAllNodes();
+            String response = data.getAsString();
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            out.write("Content-Type: application/json\r\n".getBytes());
+            out.write("\r\n".getBytes());
+            out.write(response.getBytes());
+        } else {
+            out.write("HTTP/1.1 405 Method Not Allowed\r\n".getBytes());
+            out.write("\r\n".getBytes());
+            out.flush();
+        }
+    };
+
+    public static HttpServer.HttpHandler nodesFromNetwork = (out, method, params) -> {
+        if (method.equals("GET")) {
+            UUID networkId;
+            try {
+                String pathVar = params.get("var1");
+                networkId = UUID.fromString(pathVar);
+            } catch (IllegalArgumentException e) {
+                out.write("HTTP/1.1 400 Bad Request\r\n".getBytes());
+                out.write("\r\n".getBytes());
+                return;
+            }
+            JsonObject data = TrackGraphSerializer.serializeNetworkNodes(networkId);
+            if (data == null) {
+                out.write("HTTP/1.1 418 I'm a teapot\r\n".getBytes());
+                out.write("\r\n".getBytes());
+                return;
+            }
+            String response = data.getAsString();
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            out.write("Content-Type: application/json\r\n".getBytes());
+            out.write("\r\n".getBytes());
+            out.write(response.getBytes());
+        } else {
+            out.write("HTTP/1.1 405 Method Not Allowed\r\n".getBytes());
+            out.write("\r\n".getBytes());
+        }
+    };
+
+    public static void init(HttpServer server) {
+        server.addHandler("/nodes", NodeRoutes.allNodes);
+        server.addHandler("/nodes/{uuid}", NodeRoutes.nodesFromNetwork);
+    }
+}
